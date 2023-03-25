b3af1182aa37d60cd1f7e0299e9f8fc60faa5891
unknown <ajaychandru0011@gmail.com>
2023-03-26 02:36:00 +0530

    array data

--- pages/post/[post].js
+++ pages/post/[post].js
@@ -7,71 +7,13 @@ import parse from "html-react-parser"
 import RecentPost from '../../components/RecentPost';
 import Header from '../../components/Header';
 
-const Post = () => {
+const Post = ({posts}) => {
   const router = useRouter()
   const { post } = router.query
-  const requiredPost=[
-    {
-    _id: "641ee7c67cac17cc32c2a8bf",
-    title: "Statecraft |lessons from ancient india",
-    category: "world",
-    imgSrc: "/images/95801346.webp",
-    postContent:"hello",
-    date: "Mar 23"
-    },
-    {
-        _id: "641ee7c67cac17cc32c2a8bf",
-        title: "Statecraft |lessons from ancient india",
-        category: "world",
-        imgSrc: "/images/95801346.webp",
-        postContent:"hello",
-        date: "Mar 23"
-        },
-        {
-            _id: "641ee7c67cac17cc32c2a8bf",
-            title: "Statecraft |lessons from ancient india",
-            category: "world",
-            imgSrc: "/images/95801346.webp",
-            postContent:"hello",
-            date: "Mar 23"
-            },
-            {
-                _id: "641ee7c67cac17cc32c2a8bf",
-                title: "Statecraft |lessons from ancient india",
-                category: "world",
-                imgSrc: "/images/95801346.webp",
-                postContent:"hello",
-                date: "Mar 23"
-                },
-                {
-                    _id: "641ee7c67cac17cc32c2a8bf",
-                    title: "Statecraft |lessons from ancient india",
-                    category: "world",
-                    imgSrc: "/images/95801346.webp",
-                    postContent:"hello",
-                    date: "Mar 23"
-                    },
-                    {
-                        _id: "641ee7c67cac17cc32c2a8bf",
-                        title: "Statecraft |lessons from ancient india",
-                        category: "world",
-                        imgSrc: "/images/95801346.webp",
-                        postContent:"hello",
-                        date: "Mar 23"
-                        },
-                        {
-                            _id: "641ee7c67cac17cc32c2a8bf",
-                            title: "Statecraft |lessons from ancient india",
-                            category: "world",
-                            imgSrc: "/images/95801346.webp",
-                            postContent:"hello",
-                            date: "Mar 23"
-                            },
-    ]
-
-  // const [requiredPost] = posts.filter((rPost) => {
-  //   return rPost._id === post
-  // })
+ 
+  const [requiredPost] = posts.filter((rPost) => {
+    return rPost._id === post
+  })
 
   return (
     <>
@@ -101,63 +43,63 @@ const Post = () => {
           </Grid>
 
         </Grid>
-        {/* <Grid item xs={2} sx={{ display: { xs: "none", sm: "none", md: "block" } }} >
+        <Grid item xs={2} sx={{ display: { xs: "none", sm: "none", md: "block" } }} >
           <RecentPost postObject={posts} />
-        </Grid> */}
+        </Grid>
 
       </Grid>
     </>
   )
 }
 
-// export async function getStaticPaths() {
+export async function getStaticPaths() {
   
 
-//   let res = await fetch("https://wtt.vercel.app/api/posts", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     mode: 'cors', // enable CORS
+  let res = await fetch("https://wtt-ajaychandru.vercel.app/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors', // enable CORS
    
-//   })
+  })
 
  
-//   let posts = await res.json();
-//   console.log(posts);
-//   const pathArray = posts.map((post) => {
-//     return ({
-//       params: {
-//         post: post._id
-//       }
-//     })
-//   })
+  let posts = await res.json();
+  console.log(posts);
+  const pathArray = posts.map((post) => {
+    return ({
+      params: {
+        post: post._id
+      }
+    })
+  })
 
-//   return {
-//     paths: pathArray,
-//     fallback: false, // can also be true or 'blocking'
-//   }
-// }
+  return {
+    paths: pathArray,
+    fallback: false, // can also be true or 'blocking'
+  }
+}
 
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
   
-//   let res = await fetch("https://wtt.vercel.app/api/posts", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     mode: 'cors', // enable CORS
-//     credentials: 'same-origin', // send cookies, if any
-//   })
-//   let posts = await res.json();
+  let res = await fetch("https://wtt-ajaychandru.vercel.app/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors', // enable CORS
+    credentials: 'same-origin', // send cookies, if any
+  })
+  let posts = await res.json();
 
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
+  return {
+    props: {
+      posts,
+    },
+  }
+}
 
 
 export default Post
