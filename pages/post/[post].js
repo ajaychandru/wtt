8c33b906c0c4157675ffd40dc2d92827d7154b2e
unknown <ajaychandru0011@gmail.com>
2023-03-26 11:01:14 +0530

    posts  page with custom domain

--- pages/post/[post].js
+++ pages/post/[post].js
@@ -7,97 +7,96 @@ import parse from "html-react-parser"
 import RecentPost from '../../components/RecentPost';
 import Header from '../../components/Header';
 
-const Post = () => {
+const Post = ({posts}) => {
   const router = useRouter()
   const { post } = router.query
-  return(<h1>hello</h1>);
-  // const [requiredPost] = posts.filter((rPost) => {
-  //   return rPost._id === post
-  // })
+  const [requiredPost] = posts.filter((rPost) => {
+    return rPost._id === post
+  })
 
-  // return (
-  //   <>
+  return (
+    <>
 
-  //     <Header />
-  //     <Grid container>
+      <Header />
+      <Grid container>
 
-  //       <Grid sx={{ padding:"0 4rem", boxShadow: 1 }} container item xs={12} md={10} >
-  //         <Grid  item xs={12}>
-  //           <Box sx={{ display: "flex", flexDirection: "column", textAlign: "center" }}>
-  //             <h1 className='postH1'>
-  //               {requiredPost.title}
-  //             </h1>
+        <Grid sx={{ padding:"0 4rem", boxShadow: 1 }} container item xs={12} md={10} >
+          <Grid  item xs={12}>
+            <Box sx={{ display: "flex", flexDirection: "column", textAlign: "center" }}>
+              <h1 className='postH1'>
+                {requiredPost.title}
+              </h1>
 
-  //             <h4>
-  //               Author: Ajay Chandru
-  //             </h4>
-
-
-  //           </Box>
-  //         </Grid>
-  //         <Grid className='postContent'  item xs={12}>
-
-  //           {parse(requiredPost.postContent)}
+              <h4>
+                Author: Ajay Chandru
+              </h4>
 
 
-  //         </Grid>
+            </Box>
+          </Grid>
+          <Grid className='postContent'  item xs={12}>
 
-  //       </Grid>
-  //       <Grid item xs={2} sx={{ display: { xs: "none", sm: "none", md: "block" } }} >
-  //         <RecentPost postObject={posts} />
-  //       </Grid>
+            {parse(requiredPost.postContent)}
 
-  //     </Grid>
-  //   </>
-  // )
+
+          </Grid>
+
+        </Grid>
+        <Grid item xs={2} sx={{ display: { xs: "none", sm: "none", md: "block" } }} >
+          <RecentPost postObject={posts} />
+        </Grid>
+
+      </Grid>
+    </>
+  )
 }
 
-// export async function getStaticPaths() {
+export async function getStaticPaths() {
   
-//   let res = await fetch("https://wtt-ajaychandru.vercel.app/api", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     mode: 'cors', // enable CORS
+  let res = await fetch("https://www.worldtechtravel.in/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors', // enable CORS
    
-//   })
+  })
 
  
-//   let posts = await res.json();
-//   console.log(posts);
-//   const pathArray = posts.map((post) => {
-//     return ({
-//       params: {
-//         post: post._id
-//       }
-//     })
-//   })
+  let posts = await res.json();
+  console.log(posts);
+  const pathArray = posts.map((post) => {
+    return ({
+      params: {
+        post: post._id
+      }
+    })
+  })
 
-//   return {
-//     paths: pathArray,
-//     fallback: false, // can also be true or 'blocking'
-//   }
-// }
+  return {
+    paths: pathArray,
+    fallback: false, // can also be true or 'blocking'
+  }
+}
 
 
-// export async function getStaticProps() {
-//   let res = await fetch("https://wtt-ajaychandru.vercel.app/api", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     mode: 'cors', // enable CORS
-//     credentials: 'same-origin', // send cookies, if any
-//   })
-//   let posts = await res.json();
+export async function getStaticProps() {
+  let res = await fetch("https://www.worldtechtravel.in/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors', // enable CORS
+    credentials: 'same-origin', // send cookies, if any
+  })
+  let posts = await res.json();
 
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
+  return {
+    props: {
+      posts,
+    },
+  }
+}
 
 
 export default Post
