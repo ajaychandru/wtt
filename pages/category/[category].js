db0a8f7fa4f4728cba5cda977890d5f705ecd6a3
unknown <ajaychandru0011@gmail.com>
2023-03-26 11:05:06 +0530

    category page with custom domain

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -5,67 +5,67 @@ import MediaCard from '../../components/Card'
 import { v4 as uuidv4 } from 'uuid';
 const { convert } = require('html-to-text');
 
-const Category = () => {
+const Category = ({posts}) => {
   const router = useRouter()
   const { category } = router.query
-  return(<h1>hello</h1>);
+ 
 
-  // const categoryPost = posts.filter((element, i) => {
-  //   return element.category === category
-  // })
+  const categoryPost = posts.filter((element, i) => {
+    return element.category === category
+  })
   
-  // return (
-  //   <>
-  //   <Header/>
-  //   <Grid p={5} container item xs={12} spacing={2}>
-  //     {categoryPost.map((post) => {
-  //       const cardContent=convert(post.postContent,{
-  //         selectors: [
-  //             { selector: 'img',format:'skip' },
-  //             {selector:'a',format:'skip'},
-  //             {selector:'br',format:'skip'},
+  return (
+    <>
+    <Header/>
+    <Grid p={5} container item xs={12} spacing={2}>
+      {categoryPost.map((post) => {
+        const cardContent=convert(post.postContent,{
+          selectors: [
+              { selector: 'img',format:'skip' },
+              {selector:'a',format:'skip'},
+              {selector:'br',format:'skip'},
               
-  //           ]
-  //     }).substring(0,150);
-  //       const key = uuidv4();
-  //       return (<Grid key={key} item xs={12} md={6}>
-  //         <MediaCard imgSrc={post.imgSrc} link={post._id} contMd={12} md={12} title={post.title} category={post.category} date={post.date} content={cardContent} />
-  //       </Grid>
-  //       )
-  //     })}
-  //   </Grid>
+            ]
+      }).substring(0,150);
+        const key = uuidv4();
+        return (<Grid key={key} item xs={12} md={6}>
+          <MediaCard imgSrc={post.imgSrc} link={post._id} contMd={12} md={12} title={post.title} category={post.category} date={post.date} content={cardContent} />
+        </Grid>
+        )
+      })}
+    </Grid>
 
-  //   </>
+    </>
 
 
 
-  // )
+  )
 }
-// export async function getStaticPaths() {
+export async function getStaticPaths() {
 
-//   return {
-//     paths: [{ params: { category: 'world' } }, { params: { category: 'tech' } }, { params: { category: 'travel' } }],
-//     fallback: false, // can also be true or 'blocking'
-//   }
-// }
+  return {
+    paths: [{ params: { category: 'world' } }, { params: { category: 'tech' } }, { params: { category: 'travel' } }],
+    fallback: false, // can also be true or 'blocking'
+  }
+}
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
 
-//   let res = await fetch("https://wtt-ajaychandru.vercel.app/api", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     mode: 'cors' // enable CORS
-//     // send cookies, if any
-//   });
-//   let posts = await res.json();
+  let res = await fetch("https://www.worldtechtravel.in/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors' // enable CORS
+    // send cookies, if any
+  });
+  let posts = await res.json();
 
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
+  return {
+    props: {
+      posts,
+    },
+  }
+}
 
 export default Category;
